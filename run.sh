f3af383d50ecc702173fa3de5457ea64d3aa5ab9
root <root@grazius-master>
2018-01-23 00:26:27 +0100

    6.7.1 modifications for non root user

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ "${1:0:1}" != '-' ]; then
+  exec "$@"
+fi
+
+echo "Starting with UID : $(id)"
+
+#exec gosu sonarqube \
+#  java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "$@"
